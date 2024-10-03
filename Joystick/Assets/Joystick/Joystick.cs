5c959fccb400edf7189baba702972fdc1a028eff
Uriel Fortes <82785719+UrielFortes@users.noreply.github.com>
2024-10-02 23:24:40 -0300

    Criação  do joystick

---
+++ Joystick/Assets/Joystick/Joystick.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    public Image bgJoystick, stick;
+    private Vector3 inputVector; // Armazena a posicao do stick
+
+    public void OnDrag(PointerEventData ped)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgJoystick.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        {
+            pos.x = pos.x / bgJoystick.rectTransform.sizeDelta.x;
+            pos.y = pos.y / bgJoystick.rectTransform.sizeDelta.y;
+
+            inputVector = new Vector3(pos.x * 2 - 1, 0, pos.y * 2 - 1);
+            if (inputVector.magnitude > 1)
+            {
+                inputVector = inputVector.normalized;
+            }
+
+            float sizeBg = bgJoystick.rectTransform.sizeDelta.x / 4;
+
+            stick.rectTransform.anchoredPosition = new Vector3(inputVector.x * sizeBg, inputVector.z * sizeBg, 0);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+    }
+
+    public void OnPointerUp(PointerEventData ped)
+    {
+        inputVector = Vector3.zero;
+        stick.rectTransform.anchoredPosition = Vector3.zero;
+    }
+}
